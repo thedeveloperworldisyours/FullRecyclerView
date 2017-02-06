b3ec571eb719a63af93449d05dda52f19ce8f1a9
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-02-06 14:53:46 +0000

    finished snap

--- app/src/main/java/com/thedeveloperworldisyours/fullrecycleview/snap/SnapFragment.java
+++ app/src/main/java/com/thedeveloperworldisyours/fullrecycleview/snap/SnapFragment.java
@@ -3,7 +3,9 @@ package com.thedeveloperworldisyours.fullrecycleview.snap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,17 +35,27 @@ public class SnapFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.snap_fragment, container, false);
 
+        RecyclerView recyclerViewVertical = (RecyclerView) view.findViewById(R.id.snap_fragment_vertical_recycler_view);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.snap_fragment_horizontal_recycler_view);
+
+        SnapHelper snapHelperVertical = new LinearSnapHelper();
+        snapHelperVertical.attachToRecyclerView(recyclerViewVertical);
+
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(recyclerView);
+
+        recyclerViewVertical.setHasFixedSize(true);
+        recyclerViewVertical.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL, false));
 
         SnapData dataThe = new SnapData("The", R.drawable.thedeveloperworldisyours);
-        SnapData dataDeveloper = new SnapData("Developer", R.drawable.thedeveloperworldisyours);
-        SnapData dataWorld = new SnapData("World", R.drawable.thedeveloperworldisyours);
+        SnapData dataDeveloper = new SnapData("Developer", R.drawable.thewordis);
+        SnapData dataWorld = new SnapData("World", R.drawable.theworldisyours);
         SnapData dataIs = new SnapData("Is", R.drawable.thedeveloperworldisyours);
-        SnapData dataYours = new SnapData("Yours", R.drawable.thedeveloperworldisyours);
-        SnapData dataCom = new SnapData(".com", R.drawable.thedeveloperworldisyours);
+        SnapData dataYours = new SnapData("Yours", R.drawable.thewordis);
+        SnapData dataCom = new SnapData(".com", R.drawable.theworldisyours);
 
         ArrayList<SnapData> list = new ArrayList<>();
 
@@ -56,6 +68,8 @@ public class SnapFragment extends Fragment {
 
         SnapHorizontalRecyclerViewAdapter adapter = new SnapHorizontalRecyclerViewAdapter(list, getActivity());
         recyclerView.setAdapter(adapter);
+
+        recyclerViewVertical.setAdapter(adapter);
 
         return view;
     }
