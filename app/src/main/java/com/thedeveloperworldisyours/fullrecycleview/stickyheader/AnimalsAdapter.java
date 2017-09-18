746145da4835bb9b4bb6a806376d96d7ae60ccd3
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-09-18 15:02:08 +0100

    starting with sticky header

---
+++ app/src/main/java/com/thedeveloperworldisyours/fullrecycleview/stickyheader/AnimalsAdapter.java
@@ -0,0 +1,65 @@
+package com.thedeveloperworldisyours.fullrecycleview.stickyheader;
+
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created by javierg on 18/09/2017.
+ */
+
+public abstract class AnimalsAdapter <VH extends RecyclerView.ViewHolder>
+        extends RecyclerView.Adapter<VH> {
+    private ArrayList<String> items = new ArrayList<String>();
+
+    public AnimalsAdapter() {
+        setHasStableIds(true);
+    }
+
+    public void add(String object) {
+        items.add(object);
+        notifyDataSetChanged();
+    }
+
+    public void add(int index, String object) {
+        items.add(index, object);
+        notifyDataSetChanged();
+    }
+
+    public void addAll(Collection<? extends String> collection) {
+        if (collection != null) {
+            items.addAll(collection);
+            notifyDataSetChanged();
+        }
+    }
+
+    public void addAll(String... items) {
+        addAll(Arrays.asList(items));
+    }
+
+    public void clear() {
+        items.clear();
+        notifyDataSetChanged();
+    }
+
+    public void remove(String object) {
+        items.remove(object);
+        notifyDataSetChanged();
+    }
+
+    public String getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).hashCode();
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+}
