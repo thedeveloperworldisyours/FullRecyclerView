8aa27886b30adfc9111afce73b49243ba55af39f
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-09-28 16:36:00 +0100

    added new design in Sticky

--- app/src/main/java/com/thedeveloperworldisyours/fullrecycleview/stickyheader/AnimalsHeadersAdapter.java
+++ app/src/main/java/com/thedeveloperworldisyours/fullrecycleview/stickyheader/AnimalsHeadersAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.thedeveloperworldisyours.fullrecycleview.R;
@@ -15,7 +16,7 @@ import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersAdapter;
  * Created by javierg on 18/09/2017.
  */
 
-public class AnimalsHeadersAdapter extends AnimalsAdapter<RecyclerView.ViewHolder>
+public class AnimalsHeadersAdapter extends AnimalsAdapter<AnimalsHeadersAdapter.DataObjectHolder>
         implements StickyRecyclerHeadersAdapter<RecyclerView.ViewHolder> {
 
     Context mContext;
@@ -24,18 +25,37 @@ public class AnimalsHeadersAdapter extends AnimalsAdapter<RecyclerView.ViewHolde
         this.mContext = mContext;
     }
 
+    static class DataObjectHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+        TextView mName;
+        TextView mLetterName;
+
+        DataObjectHolder(View itemView) {
+            super(itemView);
+            mName = (TextView) itemView.findViewById(R.id.contact_name_textView);
+            mLetterName = (TextView) itemView.findViewById(R.id.contact_letter);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public AnimalsHeadersAdapter.DataObjectHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.sticky_item, parent, false);
-        return new RecyclerView.ViewHolder(view) {
+        return new AnimalsHeadersAdapter.DataObjectHolder(view) {
         };
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        TextView textView = (TextView) holder.itemView;
-        textView.setText(getItem(position));
+    public void onBindViewHolder(AnimalsHeadersAdapter.DataObjectHolder holder, int position) {
+
+        holder.mName.setText(getItem(position));
+        holder.mLetterName.setText(String.valueOf(getItem(position).charAt(0)));
     }
 
     @Override
